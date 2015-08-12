ba638fbb824051bce2f02bff31fee0ad8c5cb3b9
gopinathdanda <gopinath.danda@gmail.com>
2015-08-12 19:06:14 -0400

    Convert data to string before output.

--- fibPattern.py
+++ fibPattern.py
@@ -7,41 +7,43 @@ cols = input('Enter number of columns: ')
 
 # Circle pattern only for now
 
-nos = str(rows*cols)
+nos = rows*cols
 name = 'Circle'
-rin = str(0.000000)
-ro = str(0.200000)
-patType = str(7)
-beam = str(1)   # 1=10pA
+rin = 0.000000
+ro = 0.200000
+patType = 7
+beam = 1   # 1=10pA
 matFile = 'C:\\xP\Pattern\si.mtr'
-depth = str(0.100000)
-dwell = str(0.000001000) # in secs
-overlap = str(50.000000)
-time = str(5) # in secs
-gis = str(0)
-epd = str(0)
-rot = str(0.000000)
-ppm = str(47.157898) # Pixels per micron
+depth = 0.100000
+dwell = 0.000001000 # in secs
+overlap = 50.000000
+time = 5 # in secs
+gis = 0
+epd = 0
+rot = 0.000000
+ppm = 47.157898 # Pixels per micron
 
-text = '[Pattern_Summary]\nVersion=2.00\nPatterns='+nos+'\n'
+dim = 100   # Dimensions of scan window
+
+text = '[Pattern_Summary]\nVersion=2.00\nPatterns='+str(nos)+'\n'
 for row in range(0,rows):
     for col in range(0,cols):
         text += '[Pattern_'+str(row+col+1)+']\n'
         text += 'Name='+name+'\n'
-        text += 'InnerRadius='+rin+'\n'
-        text += 'OuterRadius='+ro+'\n'
-        text += 'CenterX='+rin+'\n'
-        text += 'CenterY='+rin+'\n'
-        text += 'Type='+patType+'\n'
-        text += 'Beam='+beam+'\n'
+        text += 'InnerRadius='+str(rin)+'\n'
+        text += 'OuterRadius='+str(ro)+'\n'
+        text += 'CenterX='+str(rin)+'\n'
+        text += 'CenterY='+str(rin)+'\n'
+        text += 'Type='+str(patType)+'\n'
+        text += 'Beam='+str(beam)+'\n'
         text += 'MaterialFile='+matFile+'\n'
-        text += 'Depth='+depth+'\n'
-        text += 'Dwell='+dwell+'\n'
-        text += 'Overlap='+overlap+'\n'
-        text += 'Time='+time+'\n'
-        text += 'GIS='+gis+'\n'
-        text += 'EPD='+epd+'\n'
-        text += 'Rotation='+rot+'\n'
-        text += 'PixelsPerMicron='+ppm+'\n'
+        text += 'Depth='+str(depth)+'\n'
+        text += 'Dwell='+str(dwell)+'\n'
+        text += 'Overlap='+str(overlap)+'\n'
+        text += 'Time='+str(time)+'\n'
+        text += 'GIS='+str(gis)+'\n'
+        text += 'EPD='+str(epd)+'\n'
+        text += 'Rotation='+str(rot)+'\n'
+        text += 'PixelsPerMicron='+str(ppm)+'\n'
 
 print text
